a2109497794e46f8881f27d9e1f7626ee2fc1da0
Eleonora Belova <eleo.belova@yandex.ru>
2024-01-31 22:48:35 +0100

    fix: excluded unnecessary lines of code

--- tests/login.test.ts
+++ tests/login.test.ts
@@ -3,7 +3,6 @@ import {test} from '../helpers/test-options'
 
 test('login with existing username and valid password', async ({ page, targetUser }) => {
   await page.goto('/');
-  //await page.goto('https://www.saucedemo.com/');
   await page.locator('[data-test="username"]').click();
   await page.locator('[data-test="username"]').fill(targetUser["username"]);
   await page.locator('[data-test="password"]').click();
@@ -12,5 +11,4 @@ test('login with existing username and valid password', async ({ page, targetUse
   const currentURL = page.url();
   expect(currentURL).toBe('https://www.saucedemo.com/inventory.html')
   await expect(page.locator('#header_container')).toContainText('Swag Labs');
-  // await expect(page).toHaveTitle(/Playwright/);
 });
